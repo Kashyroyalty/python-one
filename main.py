89ae28f96828caf38b20f97989d2ca650ab8acbe
eMobilis <fathiabdikadir09@gmail.com>
2023-02-06 12:45:34 +0300

    second commit

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
 num1 =50
 num2 =100
-
-
-
-
-
 print(num1+num2)
--- second.py
+++ second.py
@@ -1,5 +1,8 @@
 firstname ="Rissah"
 secondname ="Gardner"
 
+yourage=30
+myage=20
 
 print(firstname+secondname)
+print(yourage +myage)
