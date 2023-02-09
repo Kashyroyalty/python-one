803bbfccacbe51d70645642da1496ac50393f740
eMobilis <fathiabdikadir09@gmail.com>
2023-02-09 12:54:08 +0300

    while loop

--- loops/whileloop.py
+++ loops/whileloop.py
@@ -1,1 +1,15 @@
-i = 1
+#i = 1
+#while i < 10:
+    #print(i)
+    #i+=1
+
+#number = 2
+#while number < 10:
+    #print(number)
+    #number+=1
+
+number = 5
+while number <51:
+    print(number)
+    number+=1
+
