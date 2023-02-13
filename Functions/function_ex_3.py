f3b5eca4d00c42a6c30b55ff6c0fe59256eaace1
eMobilis <fathiabdikadir09@gmail.com>
2023-02-13 12:32:49 +0300

    multiplictions

--- Functions/function_ex_3.py
+++ Functions/function_ex_3.py
@@ -1,5 +1,9 @@
-def myageinmonths():
-    age =int(input("Enter your age"))
-    months_lived =age * 12
-    print(f"you have lived for {months_lived} months")
-myageinmonths()
+try:
+    def myageinmonths():
+        age =int(input("Enter your age"))
+        months_lived =age * 12
+        print(f"you have lived for {months_lived} months")
+
+    myageinmonths()
+except:
+   print("Not a number")
