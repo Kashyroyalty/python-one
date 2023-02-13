4d3f79f66bf7ce8377e8ca030f0764771c57ec7c
eMobilis <fathiabdikadir09@gmail.com>
2023-02-13 13:14:45 +0300

    pyhon modules

---
+++ Functions/string_params.py
@@ -0,0 +1,3 @@
+def displayname(firstname):
+    print(firstname+ "Roberts")
+displayname(firstname= input("enter firstname"))
---
+++ modules/module_two.py
@@ -0,0 +1,4 @@
+from module_one import *
+
+print(addtwonumbers(30,20))
+print(multiply_two_numbers(40,20))
